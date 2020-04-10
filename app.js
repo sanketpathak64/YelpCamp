d5f6df65529f0394521eca7e02166e73db5bda7f
sanket pathak <sanketpathak64@gmail.com>
2020-04-10 19:24:41 +0530

    modified app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var campgroundRoutes = require("./routes/campgrounds.js");
 var commentRoutes = require("./routes/comments.js");
 
 
-mongoose.connect("mongodb://localhost/yelp_camp_12", { useNewUrlParser: true }); // connected to yelp_camp_v7 database
+mongoose.connect("mongodb+srv://sanket:<mongodb#321>@cluster0-ko4be.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true }); // connected to yelp_camp_v7 database
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public")); // __dirname - directory where script was run
 app.use(methodOverride("_method"));
